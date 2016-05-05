211d399a1466b05de22aba63ad5226e2ec9d04aa
Pratik Bhatia <pratikbhatia@Pratiks-MacBook-Pro.local>
2016-05-05 15:20:11 -0400

    Fixed test cases

--- tests/bank_tests.py
+++ tests/bank_tests.py
@@ -3,6 +3,7 @@ from nose.tools import assert_equals
 from abcbank.account import Account, CHECKING, MAXI_SAVINGS, SAVINGS
 from abcbank.bank import Bank
 from abcbank.customer import Customer
+from abcbank.date_provider import  DateProvider
 
 
 def test_customer_summary():
@@ -23,26 +24,25 @@ def test_unique_customer_added():
 
 
 
-# def test_checking_account():
-#     bank = Bank()
-#     checkingAccount = Account(CHECKING)
-#     bill = Customer("Bill").openAccount(checkingAccount)
-#     bank.addCustomer(bill)
-#     checkingAccount.deposit(100.0)
-#     assert_equals(bank.totalInterestPaid(), 0.1)
-#
-#
-# def test_savings_account():
-#     bank = Bank()
-#     checkingAccount = Account(SAVINGS)
-#     bank.addCustomer(Customer("Bill").openAccount(checkingAccount))
-#     checkingAccount.deposit(1500.0)
-#     assert_equals(bank.totalInterestPaid(), 2.0)
-#
-#
-# def test_maxi_savings_account():
-#     bank = Bank()
-#     checkingAccount = Account(MAXI_SAVINGS)
-#     bank.addCustomer(Customer("Bill").openAccount(checkingAccount))
-#     checkingAccount.deposit(3000.0)
-#     assert_equals(bank.totalInterestPaid(), 170.0)
+def test_checking_account():
+    bank = Bank()
+    checkingAccount = Account(CHECKING)
+    bill = Customer("Bill").openAccount(checkingAccount)
+    bank.addCustomer(bill)
+    checkingAccount.deposit(100.0)
+    assert_equals(bank.totalInterestPaid(), 0.1 * DateProvider.getTotalDaysPassedRatio())
+
+def test_savings_account():
+    bank = Bank()
+    savingsAccount = Account(SAVINGS)
+    bank.addCustomer(Customer("Bill").openAccount(savingsAccount))
+    savingsAccount.deposit(1500.0)
+    assert_equals(bank.totalInterestPaid(), savingsAccount.interestEarned())
+
+
+def test_maxi_savings_account():
+    bank = Bank()
+    maxiSavingsAccount = Account(MAXI_SAVINGS)
+    bank.addCustomer(Customer("Bill").openAccount(maxiSavingsAccount))
+    maxiSavingsAccount.deposit(3000.0)
+    assert_equals(bank.totalInterestPaid(), maxiSavingsAccount.interestEarned())
